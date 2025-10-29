2deefd93307dd974fb162b84722db7c1a2474b40
yinxl <1209554955@qq.com>
2025-10-29 21:49:47 +0800

    feat: 新增nacos使用样例

---
+++ src/main/java/com/yinxl/learning/springdemo/config/NacosDemo.java
@@ -0,0 +1,20 @@
+package com.yinxl.learning.springdemo.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class NacosDemo {
+    @Value("${user.name}")
+    private String userName;
+    @Value("${user.age}")
+    private String userAge;
+
+    @PostConstruct
+    public void init() {
+        log.info("userName:{},userAge:{}",userName,userAge);
+    }
+}
