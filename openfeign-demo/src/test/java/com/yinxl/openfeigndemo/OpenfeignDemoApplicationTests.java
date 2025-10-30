9513b7b8340d1c70019d59edbed7050f018c9653
yinxl <1209554944@qq.com>
2025-10-30 16:12:16 +0800

    feat: fegin 学习

---
+++ openfeign-demo/src/test/java/com/yinxl/openfeigndemo/OpenfeignDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yinxl.openfeigndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfeignDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
